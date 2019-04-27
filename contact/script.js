1ec70e5a66c915eb4d3314da276306382fde2dd5
vandeveldejudith <judithvandevelde2000@gmail.com>
2019-04-27 09:28:51 +0200

    Version 1

---
+++ contact/script.js
@@ -0,0 +1,60 @@
+let darkLink = document.querySelectorAll("footer a")[0];
+let lightLink = document.querySelectorAll("footer a")[1];
+let mijnCSS = document.querySelector("#switchcss");
+
+function switchToLight() {
+	mijnCSS.setAttribute("href","stijl-light.css");
+}
+function switchToDark() {
+	mijnCSS.setAttribute("href","stijl-dark.css");
+}
+lightLink.addEventListener("click",switchToLight);
+darkLink.addEventListener("click",switchToDark);
+
+// menuknop
+let menuKnop = document.querySelector(".icon");
+let navMenu = document.querySelector("nav");
+
+$(document).ready(function(){
+	$('.icon').click(function() {
+		$('.icon').toggleClass('active');
+		navMenu.classList.toggle('onzichtbaar');
+	})
+})
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
+	var slides = document.querySelectorAll(".slider li");
+	console.log(slides);
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.opacity = "0";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) { slideIndex = 1 }
+	slides[slideIndex - 1].style.opacity = "1";
+	setTimeout(showSlides, 5000);
+}
+
+/*// contactform
+$('.contactform').submit(function (e) {
+let name = document.querySelector('#naam');
+let email = document.querySelector('#email');
+let message = document.querySelector('#bericht');
+if (!name.value || !email.value || !message.value) {
+	alertify.error('kijk na of u alles heeft ingevuld.');
+} else {
+	$.ajax({
+		url: "https://formspree.io/judithvandevelde2000@gmail.com"
+		method: "POST"
+		data: $(this).serialize(),
+		dataType: "json"
+	})
+
+e.preventDefault()
+$(this).get(0).reset()
+alertify.success('bericht verzonden')
+}
+})*/
---
+++ wiebenik/script.js
@@ -0,0 +1,39 @@
+let darkLink = document.querySelectorAll("footer a")[0];
+let lightLink = document.querySelectorAll("footer a")[1];
+let mijnCSS = document.querySelector("#switchcss");
+
+function switchToLight() {
+	mijnCSS.setAttribute("href","stijl-light.css");
+}
+function switchToDark() {
+	mijnCSS.setAttribute("href","stijl-dark.css");
+}
+lightLink.addEventListener("click",switchToLight);
+darkLink.addEventListener("click",switchToDark);
+
+// menuknop
+let menuKnop = document.querySelector(".icon");
+let navMenu = document.querySelector("nav");
+
+$(document).ready(function(){
+	$('.icon').click(function() {
+		$('.icon').toggleClass('active');
+		navMenu.classList.toggle('onzichtbaar');
+	})
+})
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
+	var slides = document.querySelectorAll(".slider li");
+	console.log(slides);
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.opacity = "0";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) { slideIndex = 1 }
+	slides[slideIndex - 1].style.opacity = "1";
+	setTimeout(showSlides, 5000);
+}
